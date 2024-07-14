39e9f9a60b29ade8bfb03d5fad7453c90e9ba6a0
elyorbekanvarov <e0275king@gmail.com>
2024-07-14 12:52:59 +0500

    1-dars

---
+++ js/index.js
@@ -0,0 +1,78 @@
+// 1 - masala
+
+// let son = + prompt('Berilgan sonni kiriting');
+// let result = son % 2;
+// console.log(result);
+
+// 2 - masala
+
+// let a = + prompt('A sonini kiriting');
+// let b = + prompt('B sonini kiriting');
+// if (a > b) {
+//     let s = a * b;
+//     console.log(sum);
+// } else if (a == b) {
+//     let p = a - b;
+//     console.log(p);
+// } else {
+//     let sum = a + b;
+//     console.log(sum);
+// }
+
+// 3 - masala
+
+// // n = 5;
+// let n = + prompt("Berilgan sonni kiriting");
+// let result = n*1*2*3*4*5;
+// console.log(result);
+
+// 4 - masala
+
+// let num1 = + prompt('Birinchi sonni kiriting');
+// let num2 = + prompt('Ikkinchi sonni kiriting');
+// let num3 = + prompt('Uchinchi sonni kiriting');
+// let result = num1 * num1 + num2 * num2 + num3 * num3;
+// console.log(result);
+
+// 5 - masala
+
+// let son = + prompt('Son kirit');
+// let daraja = + prompt("Darajasini kirit");
+// let natija = Math.pov(son, daraja);
+// console.log(natija);
+
+// 7 - masala
+
+// let son1 = + prompt('Birinchi sonni kirit');
+// let son2 = + prompt("Ikkinchi sonni kirit");
+// let son1Kopaytir = son1 * 5;
+// console.log(son1Kopaytir);
+// let son2Kopaytir = son2 * 5;
+// console.log(son2Kopaytir);
+
+// 8 - masala
+
+// let son = + prompt("Son kirit");
+// let kopaytir = son * 10;
+// let natija = kopaytir - 7;
+// console.log(natija);
+
+// Integer 1
+
+// let sm = +prompt("Uzunlikni sm da kiriting");
+// let result = Math.trunc(sm / 100);
+// console.log(result);
+
+// Integer 2
+
+// let M = + prompt("O'g'irlikni kg da kiriting");
+// let natija = Math.trunc(M/1000);
+// console.log(natija);
+
+// Integer 7
+
+// let num =+prompt('2 xonali sonni kiriting');
+// let birlar = num % 10;
+// let onlar = Math.trunc(num / 10);
+// let result = birlar * 10 + onlar;
+// console.log(result);
